93137b7e922756b39425e64868685ddce2d18bda
BarZ <andy820621@gmail.com>
2024-09-13 03:09:53 +0800

    fix: fix unocss alert by adding root element to default layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,15 +7,23 @@
 </script>
 
 <template>
-  <NavBar />
+  <div class="gd-container font-spacegrotesk">
+    <NavBar />
 
-  <main class="px-7 py-10">
-    <slot />
-  </main>
+    <main class="px-7 py-10">
+      <slot />
+    </main>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 body {
   @apply overflow-x-hidden;
+}
+
+.gd-container {
+  display: grid;
+  grid-template-rows: 1fr auto;
+  min-height: 100vh;
 }
 </style>
