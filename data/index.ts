fea89860c762a933432ae5bd9fec73b9eb108498
BarZ <andy820621@gmail.com>
2024-12-31 20:12:19 +0800

    chore: Delete redundant data.

--- data/index.ts
+++ data/index.ts
@@ -2,29 +2,6 @@ export const navbarData = {
   homeTitle: 'BarZ Hsieh',
 }
 
-export const footerData = {
-  author: 'BarZ Hsieh',
-  aboutAuthor: 'Hi! I am BarZ Hsieh, a Tech enthusiast, problem solver and software engineer. Currently working at Appscode Inc.',
-  authorInterest: 'I have a fair amount of knowledge of Javascript, Typescript, VueJs, and Nuxt. If you have an interesting idea, either open source or paid let\'s connect.',
-  aboutTheSite: 'This is a personal blog site built with Nuxt3, TailwindCSS, NuxtContent, Nuxt Icon.',
-}
-
-export const homePage = {
-  title: 'Welcome To My Personal WebSite',
-  description: 'Get Web Development, Javascript, Typescript, NodeJs, Vue, and Nuxt, Related Articles, Tips, Learning resources and more.',
-}
-
-export const categoryPage = {
-  title: 'Categories',
-  description: 'Blow this category is generated from all the tags are mentioned in the different blog post',
-}
-
-export const aboutPage = {
-  title: 'BarZ Hsieh',
-  description: 'Software Engineer, Problem Solver, Web Enthusiast.',
-  aboutMe: 'Hello, fellow human! I\'m a software wizard who spends most of his day crafting code spells at @AppsCode in the Bytebuilders team. When I\'m not crafting code, you can find me summoning solutions to problems with contact links. Just don\'t ask me to cast any love spells, my magic only works on machines!',
-}
-
 export const seoData = {
   description: 'BarZ Hsieh, Software Engineer at AppsCode, with over 2.5+ years experience in software development.',
   ogTitle: 'BarZ Hsieh codes and helps others learn Javascript, Typescript, Vue, Nuxt, & Problem Solving',
