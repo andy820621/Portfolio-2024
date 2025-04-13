52fab67661b4f26001297f053cbbd3f51456387b
BarZ <andy820621@gmail.com>
2025-04-13 08:19:18 +0800

    fix: try to fix LightBox not show correct in netlify.

--- components/content/ProjectLightBox.vue
+++ components/content/ProjectLightBox.vue
@@ -5,11 +5,17 @@ const { folder } = defineProps<{
   folder: string
 }>()
 
-const { data: photos } = await useFetch<Photo[]>('/api/project-images', {
+const { data: photos, error } = await useFetch<Photo[]>('/api/project-images', {
   query: { folder },
 })
 </script>
 
 <template>
   <LightBox v-if="photos && photos.length" :photos="photos" />
+  <div v-else-if="photos && !photos.length">
+    No images found.
+  </div>
+  <div v-else>
+    ProjectLightBox Component has an error: {{ error }}
+  </div>
 </template>
