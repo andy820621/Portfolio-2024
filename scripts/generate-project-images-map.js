7ef646640d1aacc61ad5a1f2c84d2295143138e1
BarZ <andy820621@gmail.com>
2025-04-22 21:35:31 +0800

    fix: fix `glob` may not support all module.exports as named exports, use destructuring from pkg.

--- scripts/generate-project-images-map.js
+++ scripts/generate-project-images-map.js
@@ -1,7 +1,9 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
+import pkg from 'glob'
+
+const { glob } = pkg
 
 // 獲取當前文件的目錄路徑
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
