7eddf3dd48c64bc045dc112c5c73d888f248339b
BarZ <andy820621@gmail.com>
2025-10-25 23:51:08 +0800

    fix: 更新 useContentDetailData 以簡化內容處理邏輯，修正內容引用方式 chore: 更新 package.json 中 @stefanobartoletti/nuxt-social-share 版本至 2.1.1 chore: 更新 pnpm-lock.yaml 中的依賴版本，確保一致性

--- app/composables/useContentDetailData.ts
+++ app/composables/useContentDetailData.ts
@@ -4,20 +4,23 @@ import type { AsyncContentDataType } from '~~/types/main'
 export function useContentDetailData<T = PageCollectionItemBase>(contentData: AsyncContentDataType) {
   const { t } = useI18n()
 
-  const { content, surroundContent } = toRefs(contentData.value!)
-
   const mainData = computed(() => {
-    const wordCount = content.value.body ? countWords(content.value.body) : 0
+    const content = contentData.value?.content
+    if (!content) {
+      return {} as T & { wordCount: number, readingTime: string }
+    }
+
+    const wordCount = content.body ? countWords(content.body) : 0
 
     return {
-      ...content.value as T,
+      ...content as T,
       wordCount,
       readingTime: useEstimateReadingTime(wordCount, t),
     }
   })
 
-  const prevContent = computed(() => surroundContent.value?.[0] ?? null)
-  const nextContent = computed(() => surroundContent.value?.[1] ?? null)
+  const prevContent = computed(() => contentData.value?.surroundContent?.[0] ?? null)
+  const nextContent = computed(() => contentData.value?.surroundContent?.[1] ?? null)
 
   return {
     mainData,
